70edd45c28db1311c55280dfe8b69d1639a00df5
GeKtoRiX <gektor93@gmail.com>
2025-10-25 15:47:56 +0300

    addeded logic/small fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,41 @@
+import { useState } from 'react';
 import Header from './components/Header';
 import UserInput from './components/UserInput';
+import Result from './components/Result';
 
 function App() {
+  // Хук отслеживания состояния вводных данный инвестирования.
+  const [userInput, setUserInput] = useState({
+    // Первоначальная сумма инвестиций.
+    initialInvestment: 1000,
+    // Годовая сумма инвестиций.
+    annualInvestment: 1200,
+    // Ожидаем процент заработка с суммы инвестиций.
+    expectedReturn: 6,
+    // Длительность инвестиции в месяцах.
+    duration: 10,
+  });
+  // Функция вызова обновления вводимых данных.
+  function handleChange(inputIdentifire, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifire]: +newValue,
+      };
+    });
+  }
+
+  const inputIsValid = userInput.duration >= 1;
+
   return (
     <>
       <Header />
       <main>
-        <UserInput />
+        <UserInput userInput={userInput} onChange={handleChange} />
+        {!inputIsValid && (
+          <p className='center'>Please enter the duration greater then zero.</p>
+        )}
+        {inputIsValid && <Result input={userInput} />}
       </main>
     </>
   );
